c896a651b6c0f19f0021f81475d4300236968cd1
Carlos Orjuela <ingkrlosorjuela@gmail.com>
2023-07-10 19:41:46 -0500

    Menu List integrated

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../domain/entities/menu_items.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -9,6 +11,47 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Page Home'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTitle(menuItem: menuItem);
+
+      },
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  final MenuItem menuItem;
+
+  const _CustomListTitle({
+    required this.menuItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      onTap: () {
+        
+      },
     );
   }
 }
